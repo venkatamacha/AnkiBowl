bcfa11dff7876220e542693ebb50b678e4f1115e
Venkata Macha <venkatamacha@gmail.com>
2020-07-29 14:51:56 -0500

    Pretty Good

--- client/game/main.js
+++ client/game/main.js
@@ -294,13 +294,13 @@ socket.on("netRes", () => {
 
     // if ping to server is greater than 250ms,
     // warn the user that their connection is poor
-    if (avgPing >= 250) {
-        // console.warn("High latency detected.");
-        // $("#warnPing").text(`High latency detected (${ms}ms). Try refreshing the page or connecting to a faster network.`);
-        // $("#warnPing").show();
+    if (avgPing >= 200) { // used to be 250
+        console.warn("High latency detected.");
+        $("#warnPing").text(`High latency detected (${ms}ms). Try refreshing the page or connecting to a faster network.`);
+        $("#warnPing").show();
     }
     else {
-        // $("#warnPing").hide();
+        $("#warnPing").hide();
     }
 
 });
